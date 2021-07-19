7fdafd7e93541382a9301fbae2f2dd37824cc1a9
Andres Briseno <72451287+AndresBriC@users.noreply.github.com>
2021-07-19 18:38:04 -0500

    Completa el lexerBeginning

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import re
+import csv
 
-
-txt = " n_1234  =400 +  2 0  " #String de pruba (Reemplazar con la lectura del archivo txt)
+tokenTable = [] #Contiene la tabla con los caracteres y tipos
+counter = 0
 
 #Separa el texto a partir del = y elimina los espacios de la expresion
 def stringSplitter(txt):
@@ -11,12 +12,10 @@ def stringSplitter(txt):
     postEqual = False #Para checar lo que va despues del =
 
     #Va checando toda la string para ver si esta antes o despues del =
-    for character in txt:
-        if character == " " and postEqual == False: #Si hay un espacio en la variable, lo agrega para analizar
-            beginning += character
-        elif character == " " and postEqual == True: #Si hay un espacio en la expresion, lo ignora
-            continue
-        elif character != "=" and postEqual == False: #Caracteres antes del =
+    for index, character in enumerate(txt):
+
+        #Separa las strings si es antes o despues la asignacion
+        if character != "=" and postEqual == False: #Caracteres antes del =
             beginning += character
         elif character == "=" and postEqual == False: #Encuentra el =
             beginning += character
@@ -28,8 +27,8 @@ def stringSplitter(txt):
             end += character
             break
     
-    if postEqual == False: #Si no se encontro un =
-        print("No hay operador de asignacion")
+    """ if postEqual == False: #Si no se encontro un =
+        print("No hay operador de asignacion") """
 
     #Agrega ambas partes a una lista
     splitString.append(beginning)
@@ -40,36 +39,73 @@ def stringSplitter(txt):
 #Hace el desglose del string hasta el operador de asignacion
 def lexerAritmeticoBeginning(txt):
     begSpace = False
+    global tokenTable
+    global counter
 
-    #La idea para checar los espacios es que si es un espacio del principio (antes de cualquier otro caracter), es valido
-    #, pero después de eso, debe checar el siguiente caracter, si es espacio o igual, es válido, si no, no
+    for index, character in enumerate(txt): #Enumerate() se usa para poder tener el indice y el caracter
+        counter += 1
 
-    for index, character in enumerate(txt):
+        #Checa si hay un operador de asignacion
         if index == len(txt)-1 and character == "=": #Si el ultimo caracter es un =
-            #Reportar el =
-            print(character + " asignación")
+            tokenTable.append(['', character, 'Asignación',counter])
         elif index == len(txt)-1 and character != "=": #Si el ultimo caracter no es un =
-            print("No hay operador de asignación")
-        elif character != " " and begSpace == False:
+            print("No hay operador de Asignación")
+        elif index != len(txt)-1 and character == "/" and txt[index+1] == "/": #Si detecta un comentario, deja de analizar el resto
+            tokenTable.append(['', '//','Comentario',counter])
+            break
+        
+        if character == " " and begSpace == False: #Ignora los espacios del principio
+            continue
+        elif character != " " and begSpace == False: #Aqui se encuentra algo despues de los espacios del principio, solo puede ser letra
             begSpace = True
-        elif character == " " and begSpace == True:
-            print("Space at invalid position")
+            if re.search("[a-zA-Z]", character) != None: #Si es una letra
+                tokenTable.append(['', character, 'Variable', counter])
+            else: #Si no es letra, manda error y deja de leer la string
+                tokenTable.append(['', character, 'Error, primer caracter diferente a una letra', counter])
+        
+        if index != len(txt)-1: #Para caracteres antes del ultimo
+            if character == " " and begSpace == True and (txt[index+1] == " " or txt[index+1] == "="): #Si el actual es espacio y el sig. es espacio o igual
+                continue
+            elif character == " " and begSpace == True and (txt[index+1] != " " or txt[index+1] != "="):
+                tokenTable.append(['', character, 'Error, espacio dentro de la variable o variable sin asignación', counter])
 
 
 def lexerAritmeticoEnd(txt):
     #Aqui pondria mi lexer para la expresion... SI TUVIERA UNO
+    opSpace = False #Checa si es un espacio despues de un operador
 
     if len(txt) == 0: #Si le pasan "end" vacio
         print("No hay operador de asignacion")
     print("Lexer end")
 
-#Testing
-twoParts = stringSplitter(txt) #Contiene el string con las partes separadas por el =
-beginning = twoParts[0]
-end = twoParts[1]
+#Abre el texto con los casos de prueba
+testsFile = open('P1_pruebas_.txt', "r")
 
-lexerAritmeticoBeginning(beginning)
+#Lee cada linea del archivo
+for line in testsFile:
+    txt = line
 
-#print(twoParts)
-print("First part: " + beginning)
-print("Second part: " + end)
+    #Testing
+    twoParts = stringSplitter(txt) #Contiene el string con las partes separadas por el =
+    beginning = twoParts[0]
+    end = twoParts[1]
+    #print(twoParts)
+    print("First part: " + beginning)
+    if end == "":
+        print("Second part: not found")
+    else:
+        print("Second part: " + end)
+
+    #Agrega los resultados de la tabla del lexer a la tabla en general
+    tokenTable.append([beginning+end,'','',counter])
+    lexerAritmeticoBeginning(beginning)
+    lexerAritmeticoEnd(end)
+
+    print("______________________________________________________________________________________\n")
+
+testsFile.close()
+
+with open('tokenTable.csv', 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(["Expresión","Token","Type","Index"])
+    writer.writerows(tokenTable)
